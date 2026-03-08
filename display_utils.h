934e941813862e27069af871930fa18077fb5a34
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-03-08 15:30:31 +0100

    humidity and temperature h

--- display_utils.h
+++
@@ -1,9 +0,0 @@
-// display_utils.h
-#ifndef DISPLAY_UTILS_H
-#define DISPLAY_UTILS_H
-
-// Function prototypes
-void display_temp_hum(unsigned char temp, unsigned char hum);
-void display_error(unsigned char error_code);
-
-#endif
